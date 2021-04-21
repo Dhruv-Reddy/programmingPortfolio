2f1de406ff20720cbb02a1afa2442046a5c543d0
Dhruv-Reddy <dhruredd9639@granitesd.org>
2021-04-21 07:58:18 -0600

    Add files via upload

---
+++ src/MadLibsBolt.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main{
+public static void main(String[] args){
+
+  Scanner input = new Scanner(System.in);
+  String name , name2, gender, verb, food, adjective, noun, noun2, place, day, toy, adjective2, adjective3;
+
+System.out.print("Type your character's Name: ");
+  name2 = input.next();
+
+  System.out.print("Type the trespasser's Name: ");
+  name = input.next();
+
+   System.out.print("Man or Women (according to trespasser): ");
+  gender = input.next();
+
+   System.out.print("Type a Verb end with ing: ");
+  verb = input.next();
+
+   System.out.print("Type a Food: ");
+  food = input.next();
+
+   System.out.print("Type a Adjective: ");
+  adjective = input.next();
+
+  System.out.print("Type a Second Adjective: ");
+  adjective2 = input.next();
+
+  System.out.print("Type a Third Adjective: ");
+  adjective3 = input.next();
+
+   System.out.print("Type a Noun: ");
+  noun = input.next();
+
+  System.out.print("Type a Second Noun: ");
+  noun2 = input.next();
+
+  System.out.print("Type a Place: ");
+  place = input.next();
+
+  System.out.print("Type a Day: ");
+  day = input.next();
+
+   System.out.print("Type a Toy: ");
+  toy = input.next();
+
+  System.out.println("Someone is outside my house. Its 3 a.m. in the morning. First let me introduce my self. My name is " + name2 + " . I have a " + adjective2 + " son and a " + adjective3 + " mother living with us in Texas. But anyway, back to the story. There is a name tag on their uniform that says " + name + ". It looks like it is a " + gender + ". " + name + " seems to be "+ verb + " the ground. " + name + " is also holding a " + food + " with their other hand. I  recognize that " + gender + " somewhere. I think they were on the news the other day. I hear and see police sirens. This is not good. Who ever " + name + " is, they need to leave. I grab my " + adjective + " son's " + noun + " and sneak outside by the back door in the kitchen. I don't think that " + name + " know that i left. " + name + " does not see me at the side of the house. But " + name + " is pulling something out of their pocket. It is a " + noun2 + "!? How does " + name + " even have that? I pull out my phone and dialed 911. While the phone is ringing, " + name + " sees me. The crazy part is that he runs full speed at me. Then it hits me. I did not see " + name + " on the news, i have seen them on the sports channel!" + name + " IS THAT USAIN BOLT'S SIBLING?! I tell " + name + " to stop which surprisingly worked. I ask " + name + " why they are doing this. " + name + " introduces themselves as " + name + " Bolt. " + name + " tells me that they are not from Jamaica unlike their famous sibling. They were born in " + place + " on a " + day + ". Weirdly, " + name + " tells me that there favorite toy was " + toy + " I ask " + name + " what is with the subway uniform. He says that he is trying to find someone that stole something from there family and does not want to tell Usain. Thus trying to make money" + name + " tells me that my  father stole something from there father. But before " + name + " can tell me what it is, the cops arrive and arrest " + name + ". Will i ever get to known what my father stole from " + name + "? 🆃🅾 🅱🅴 🅲🅾🅽🆃🅸🅽🆄🅴🅳");
+}
+
+}
